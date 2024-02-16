b8f1e801ddf8e5f70816825a80a6d8bcaa8c64a1
Saif <ayeshsaif367@gmail.com>
2024-02-17 00:57:36 +0300

    ss

--- Mandelbrot/settings.py
+++ Mandelbrot/settings.py
@@ -8,7 +8,7 @@ ENV = environ.Env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve()
+BASE_DIR = Path(__file__).resolve().parent
 
 environ.Env.read_env(BASE_DIR / '.env')
 
