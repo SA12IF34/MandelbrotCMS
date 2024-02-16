eea6af85c56b1eac08549dfc7a184da20129673f
Saif <ayeshsaif367@gmail.com>
2024-02-17 01:18:25 +0300

    ss

--- Mandelbrot/settings.py
+++ Mandelbrot/settings.py
@@ -154,7 +154,7 @@ ROOT_URLCONF = 'Mandelbrot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'FrontEnd')],
+        'DIRS': [os.path.join(BASE_DIR, 'FrontEnd'), os.path.join('/app', 'FrontEnd')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -223,7 +223,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Frontend/entertainment/dist/assets'),
     os.path.join(BASE_DIR, 'FrontEnd/goals/dist/assets'),
     os.path.join(BASE_DIR, 'FrontEnd/home-tasks/dist/assets'),
-    os.path.join(BASE_DIR, 'FrontEnd/website-home/dist/assets')
+    os.path.join(BASE_DIR, 'FrontEnd/website-home/dist/assets'),
+    os.path.join('/app', 'FrontEnd/sessions-manager/dist/assets'),
+    os.path.join('/app', 'FrontEnd/learning-tracker/dist/assets'),
+    os.path.join('/app', 'Frontend/entertainment/dist/assets'),
+    os.path.join('/app', 'FrontEnd/goals/dist/assets'),
+    os.path.join('/app', 'FrontEnd/home-tasks/dist/assets'),
+    os.path.join('/app', 'FrontEnd/website-home/dist/assets')
     
 ]
 
