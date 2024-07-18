49a2fe3374d6b83983f94345237cd07dc2552b58
Saif <ayeshsaif367@gmail.com>
2024-07-18 14:19:36 +0300

    Commit!

--- Mandelbrot/settings.py
+++ Mandelbrot/settings.py
@@ -216,25 +216,25 @@ WSGI_APPLICATION = 'Mandelbrot.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': ENV('PG_NAME'), 
-            'USER': ENV('PG_USER'),
-            'PASSWORD': ENV('PG_PASSWORD'),
-            'HOST': ENV('PG_HOST'), 
-            'PORT': ENV('PG_PORT'),
-        }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': ENV('PG_NAME'), 
+        'USER': ENV('PG_USER'),
+        'PASSWORD': ENV('PG_PASSWORD'),
+        'HOST': ENV('PG_HOST'), 
+        'PORT': ENV('PG_PORT'),
     }
+}
 
 
 
