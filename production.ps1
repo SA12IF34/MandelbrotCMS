162e721e7b42fa5e17e38576b4db4db210ce5be8
Saif <ayeshsaif367@gmail.com>
2024-08-15 15:53:12 +0300

    commit

--- production.ps1
+++ production.ps1
@@ -1,45 +1,36 @@
 Set-Location -Path ".\FrontEnd\entertainment"
-npm install
 vite build --mode production
 
 Set-Location -Path ".."
 
 Set-Location -Path ".\goals"
-npm install
 vite build --mode production
 
 Set-Location -Path ".."
 
 Set-Location -Path ".\home-tasks"
-npm install
 vite build --mode production
 
 Set-Location -Path ".."
 
 Set-Location -Path ".\learning-tracker"
-npm install
 vite build --mode production
 
 Set-Location -Path ".."
 
 Set-Location -Path ".\sessions-manager"
-npm install
 vite build --mode production
 
 Set-Location -Path '..'
 
 Set-Location -Path ".\notes"
-npm install
 vite build --mode production
 
 Set-Location -Path '..'
 
 Set-Location -Path ".\website-home"
-npm install
 vite build --mode production
 
 
 Set-Location -Path "..\.."
-# git add .
-# git commit -m "commit"
-# git push origin main
+
